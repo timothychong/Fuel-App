6c76078377860d26dff296077e538d747b2b797b
Timothy Chong <timothychong@outlook.com>
2014-02-08 20:38:35 -0500

    added detail challenge

---
+++ Fuel/Fuel/FLDetailChallengeViewController.m
@@ -0,0 +1,38 @@
+//
+//  FLDetailChallengeViewController.m
+//  Fuel
+//
+//  Created by Timothy Chong on 2/8/14.
+//  Copyright (c) 2014 Fuel. All rights reserved.
+//
+
+#import "FLDetailChallengeViewController.h"
+
+@interface FLDetailChallengeViewController ()
+
+@end
+
+@implementation FLDetailChallengeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
